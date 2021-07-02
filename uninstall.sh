350e58ade68cfd3dd523ba273a2b4c90d29e576e
linxuekai <linxuekai@qq.com>
2021-07-02 10:58:43 +0800

    提取公共函数3

--- uninstall.sh
+++ uninstall.sh
@@ -1,6 +1,21 @@
-#!/bin/bash
+#/bin/bash
 
-. ./funcs.sh
+# @func - 封装出错返回
+# @param $1 - 错误提示
+exit_if_err() {
+    RET=$?
+    [ $RET -eq 0 ] || {
+        echo $1
+        exit $RET
+    }
+    unset RET
+}
+
+# @func - 需要权限时检查如果不是 root 运行就退出
+check_permission() {
+    [ "root" = "$(whoami)" ]
+    exit_if_err '非 root 请使用 sudo 运行'
+}
 
 # @func - 需要权限时检查如果不是 root 运行就退出
 check_permission
